d7234368953480d45aa99fe121fc7feda1b4ac83
Tyler Pearson <tdpearson@users.noreply.github.com>
2018-04-10 13:11:49 -0500

    add lxml and tick version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 #ez_setup.use_setuptools()
 from setuptools import setup, find_packages
 setup(name='recipewriterq',
-      version='0.3.2',
+      version='0.3.3',
       packages= find_packages(),
       install_requires=[
           'celery==3.1.22',
@@ -10,5 +10,6 @@ setup(name='recipewriterq',
           'requests==2.13.0',
           'PyYAML==3.12',
           'boto3',
+          'lxml',
       ],
 )
