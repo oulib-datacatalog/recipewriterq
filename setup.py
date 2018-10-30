9dc75751756f81d0390dd3bf16ba5007ab6f5afa
Tyler Pearson <tdpearson@users.noreply.github.com>
2018-10-30 18:28:47 -0500

    update dependency

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@
 #ez_setup.use_setuptools()
 from setuptools import setup, find_packages
 setup(name='recipewriterq',
-      version='0.3.3',
+      version='0.3.4',
       packages= find_packages(),
       install_requires=[
           'celery==3.1.22',
           'bagit==1.5.4',
-          'requests==2.13.0',
+          'requests==2.20.0',
           'PyYAML==3.12',
           'boto3',
           'lxml',
