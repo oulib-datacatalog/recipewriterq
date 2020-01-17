61f4ca3df9d7c4ab5ed2c6b8d1b9bef0cd67fe1e
Tyler Pearson <tdpearson@users.noreply.github.com>
2020-01-17 13:31:31 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ setup(name='recipewriterq',
       version='0.3.2',
       packages= find_packages(),
       install_requires=[
-          'celery==3.1.22',
           'bagit==1.5.4',
           'requests==2.13.0',
           'PyYAML==3.12',
