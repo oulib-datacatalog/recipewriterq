1843d94747b3594171fd99ffae999d3a8b9a3afc
Tyler Pearson <tdpearson@users.noreply.github.com>
2020-08-04 18:07:05 -0500

    upgrade requests

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@
 #ez_setup.use_setuptools()
 from setuptools import setup, find_packages
 setup(name='recipewriterq',
-      version='0.3.4',
+      version='0.3.5',
       packages= find_packages(),
       install_requires=[
           'celery==3.1.22',
           'bagit==1.5.4',
-          'requests==2.20.0',
+          'requests==2.24.0',
           'PyYAML==3.12',
           'boto3',
           'lxml',
